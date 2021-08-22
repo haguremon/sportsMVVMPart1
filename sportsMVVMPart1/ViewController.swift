5dc00c40f16b2ba5578228f82b56a2655d089540
Yuta iwasaki <yanyong0127@gmail.com>
2021-08-22 17:31:58 +0900

    Initial Commit

---
+++ sportsMVVMPart1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sportsMVVMPart1
+//
+//  Created by IwasakIYuta on 2021/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
